dfec38332c4082d2a3c28bf3fbe95ca98e3961cf
Håvard Neset <havard.neset@gmail.com>
2023-12-02 13:50:46 +0100

    day 2 part 2 - remove unused code

--- src/main/kotlin/puzzles/day2/Day2Part2.kt
+++ src/main/kotlin/puzzles/day2/Day2Part2.kt
@@ -7,9 +7,6 @@ import file.ResourceFile
 class Day2Part2: Puzzle {
 
     private val input = ResourceFile("day_2_input.txt")
-    private val maxRed = 12
-    private val maxGreen = 13
-    private val maxBlue = 14
 
     override fun solve(): PuzzleResult =
         input.lines()
