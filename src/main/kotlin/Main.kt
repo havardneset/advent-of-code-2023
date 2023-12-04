63e17da66563e520ba5b109a3326c78b082e764c
Håvard Neset <havard.neset@schibsted.com>
2023-12-04 18:04:24 +0100

    Day 4 start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ import puzzles.day2.Day2Part1
 import puzzles.day2.Day2Part2
 import puzzles.day3.Day3Part1
 import puzzles.day3.Day3Part2
+import puzzles.day4.Day4Part1
 import kotlin.time.measureTimedValue
 
 fun main() {
@@ -11,7 +12,8 @@ fun main() {
     val puzzles = listOf(
         Day1Part1(), Day1Part2(),
         Day2Part1(), Day2Part2(),
-        Day3Part1(), Day3Part2()
+        Day3Part1(), Day3Part2(),
+        Day4Part1()
     )
 
     puzzles.forEach { puzzle ->
